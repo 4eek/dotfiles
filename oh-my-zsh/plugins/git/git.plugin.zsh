05fc62ff61becf7765daa23993eff5ce17fa6f20
Kevin G. Fourie <kevin@4e.co.za>
2012-07-16 12:27:23 +0200

    Updated OMZ.

--- oh-my-zsh/plugins/git/git.plugin.zsh
+++ oh-my-zsh/plugins/git/git.plugin.zsh
@@ -1,4 +1,6 @@
 # Aliases
+alias g='git'
+compdef g=git
 alias gst='git status'
 compdef _git gst=git-status
 alias gl='git pull'
@@ -50,6 +52,12 @@ alias gsd='git svn dcommit'
 function current_branch() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
 }
 
 # these aliases take advantage of the previous function
